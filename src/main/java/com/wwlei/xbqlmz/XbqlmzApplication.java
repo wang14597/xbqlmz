a89fe392c0101ad3660d9a8aaa02b35ce6bd0481
Lei <1459703155@qq.com>
2024-05-14 19:30:42 +0800

    init

---
+++ src/main/java/com/wwlei/xbqlmz/XbqlmzApplication.java
@@ -0,0 +1,13 @@
+package com.wwlei.xbqlmz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbqlmzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XbqlmzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wwlei/xbqlmz/XbqlmzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwlei.xbqlmz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbqlmzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
