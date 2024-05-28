687728c60821d824ffcea8cc635dc52e9c5a6780
Lei <1459703155@qq.com>
2024-05-28 11:25:29 +0800

    fix register Jedis bean issue

--- src/main/java/com/wwlei/xbqlmz/domain/component/config/redis/RedisConfig.java
+++ src/main/java/com/wwlei/xbqlmz/domain/component/config/redis/RedisConfig.java
@@ -1,9 +1,12 @@
 package com.wwlei.xbqlmz.domain.component.config.redis;
 
 import lombok.Data;
+import org.springframework.context.annotation.Bean;
 import org.springframework.lang.Nullable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Max;
@@ -27,4 +30,23 @@ public class RedisConfig {
     private int poolMaxWait;
     @Min(0)
     private int database;
+
+    /**
+     * create JedisPool instance。
+     *
+     * @return JedisPool。
+     */
+    @Bean
+    protected JedisPool jedisPool() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxIdle(this.getPoolMaxIdle());
+        poolConfig.setMaxTotal(this.getPoolMaxTotal());
+        poolConfig.setMaxWaitMillis(this.getPoolMaxWait() * 1000L);
+
+        // https://github.com/redis/jedis/issues/2781#issuecomment-1092744636
+        poolConfig.setJmxEnabled(false);
+
+        return new JedisPool(poolConfig, this.getHost(), this.getPort(),
+                this.getTimeout() * 1000, this.getPassword(), this.getDatabase());
+    }
 }
