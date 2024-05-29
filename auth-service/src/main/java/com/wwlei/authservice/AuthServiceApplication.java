6e829ba094eacd0dfc3532eb1cd1113945a541de
Lei <1459703155@qq.com>
2024-05-29 13:14:40 +0800

    add GlobalExceptionHandler and ApiResponse, add component scan for auth-service

--- auth-service/src/main/java/com/wwlei/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/wwlei/authservice/AuthServiceApplication.java
@@ -1,11 +1,16 @@
 package com.wwlei.authservice;
 
+import com.wwlei.common.utils.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+@ComponentScan(basePackages = {AuthServiceApplication.APPLICATION_PACKAGE_NAME, Constants.GLOBAL_EXCEPTION_PACKAGE_NAME})
 public class AuthServiceApplication {
+
+    public static final String APPLICATION_PACKAGE_NAME = "com.wwlei.authservice";
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
