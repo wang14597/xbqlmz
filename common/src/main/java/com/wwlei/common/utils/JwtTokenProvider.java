cc73b167ca7abb0d357cc079d589453df4753f9b
Lei <1459703155@qq.com>
2024-06-05 17:31:07 +0800

    add create jwt token

---
+++ common/src/main/java/com/wwlei/common/utils/JwtTokenProvider.java
@@ -0,0 +1,50 @@
+package com.wwlei.common.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class JwtTokenProvider {
+
+    private JwtTokenProvider() {}
+
+    private final static Key KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public static String createToken(String username, String role) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1800000)) // 30 minutes
+                .signWith(KEY)
+                .compact();
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(KEY).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(KEY).build().parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+
+    public static String getRoleFromToken(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(KEY).build().parseClaimsJws(token).getBody();
+        return claims.get("role", String.class);
+    }
+}
