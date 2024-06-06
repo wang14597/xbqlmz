4aae52b2ef8022e2618d617a3ec9f1c4ded8d42c
Lei <1459703155@qq.com>
2024-06-06 19:50:42 +0800

    add read and write permission control

--- gateway/src/main/java/com/wwlei/gateway/security/SecurityConfig.java
+++ gateway/src/main/java/com/wwlei/gateway/security/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.wwlei.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,13 +24,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/auth/login").permitAll()
+        http.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/auth/login", "/auth/registry").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/auth/**")
+                        .access(hasAuthority("READ"))
+                        .pathMatchers("/auth/**")
+                        .access(hasAuthority("WRITE"))
                         .anyExchange().authenticated()
                 )
                 .addFilterAt(jwtTokenAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .csrf().disable();
         return http.build();
+    }
+
+    private ReactiveAuthorizationManager<AuthorizationContext> hasAuthority(String authority) {
+        return (authentication, context) -> authentication.map(auth ->
+                auth.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .anyMatch(authority::equals)
+        ).map(AuthorizationDecision::new);
     }
 }
